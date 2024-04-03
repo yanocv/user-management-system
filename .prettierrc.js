21431c72b6f1c33f7f93990c1069a7cb6954c78f
Hayato Yano <51319482+yanocv@users.noreply.github.com>
2024-04-03 19:43:32 +0900

    Add: Server sonfig files
    Server

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   semi: true,
   trailingComma: "es5",
-  singleQuote: false,
+  singleQuote: true,
   printWidth: 80,
   tabWidth: 2,
   quoteProps: "as-needed",
   bracketSpacing: true,
-  bracketSameLine: false,
+  jsxBracketSameLine: false,
   jsxSingleQuote: false,
   endOfLine: "auto",
   arrowParens: "always",
-  useTabs: false,
+  useTabs: false
 };

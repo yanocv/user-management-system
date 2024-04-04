e825f5ebfbf22b1240f38d61694e89907b1f54ab
Hayato Yano <51319482+yanocv@users.noreply.github.com>
2024-04-04 14:59:02 +0900

    Delete server directory

--- server/src/routes/versions/v1/company/index.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { CompanyController } from '@controllers/company';
-
-const router = express.Router();
-
-/**　会社一覧情報を取得する.　*/
-router.get('/', CompanyController.findAll);
-
-export { router as CompanyRouter };
